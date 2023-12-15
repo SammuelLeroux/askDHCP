19ec1b0341bdde55ba0b982d97a3d58c60c1025d
SammuelLeroux <sammuel.leroux@epitech.eu>
2023-12-15 14:31:30 +0100

    Maj pathing

--- init.sh
+++ init.sh
@@ -17,7 +17,7 @@ fi
 # Télécharge le repo GitHub avec wget
 echo "Téléchargement du repo GitHub..."
 wget -O "$repo_dir/askdhcp.zip" https://github.com/SammuelLeroux/askDHCP/archive/main.zip
-unzip "$repo_dir/askdhcp.zip" -d "$repo_dir/"
+unzip "$repo_dir/askdhcp.zip" -d "$repo_dir"
 mv "$repo_dir/askDHCP-main" "$repo_dir/askdhcp"
 rm "$repo_dir/askdhcp.zip"
 
@@ -26,8 +26,8 @@ echo "Ajout de la commande dans /etc/rc.local..."
 echo "$repo_dir/askdhcp/config_intnet.sh" | sudo tee -a /etc/rc.local
 
 # Exécute le script config_intnet.sh immédiatement
-chmod +x "$repo_dir/askdhcp/config_intnet.sh"
-"$repo_dir/askdhcp/config_intnet.sh"
+chmod +x "$repo_dir/askdhcp/scripts/config_intnet.sh"
+"$repo_dir/askdhcp/scripts/config_intnet.sh"
 
 # Exécute le script deploy_all.sh
 chmod +x "$repo_dir/askdhcp/deploy_all.sh"
